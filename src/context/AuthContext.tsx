af97dd3b97390bf81c9149b2b03067c9771680f4
milliorn <scottmilliorn@gmai.com>
2023-06-09 22:30:55 -0700

    Listen for Authentication Changes

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import firebase_app from '@/firebase/config';
+
+// Initialize Firebase auth instance
+const auth = getAuth( firebase_app );
+
+// Create the authentication context
+export const AuthContext = createContext( {} );
+
+// Custom hook to access the authentication context
+export const useAuthContext = () => useContext( AuthContext );
+
+interface AuthContextProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContextProvider = ( { children }: AuthContextProviderProps ) => {
+  // Set up state to track the authenticated user and loading status
+  const [ user, setUser ] = useState<User | null>( null );
+  const [ loading, setLoading ] = useState( true );
+
+  useEffect( () => {
+    // Subscribe to the authentication state changes
+    const unsubscribe = onAuthStateChanged( auth, ( user ) => {
+      if ( user ) {
+        // User is signed in
+        setUser( user );
+      } else {
+        // User is signed out
+        setUser( null );
+      }
+      // Set loading to false once authentication state is determined
+      setLoading( false );
+    } );
+
+    // Unsubscribe from the authentication state changes when the component is unmounted
+    return () => unsubscribe();
+  }, [] );
+
+  // Provide the authentication context to child components
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  );
+};
