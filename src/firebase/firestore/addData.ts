8debba8730ca586db3b821bb1e356070126f8b0e
Fhima Anoir <57225309+Anoirwork@users.noreply.github.com>
2024-04-20 15:17:47 +0100

    Update addData.ts
    Signed-off-by: Fhima Anoir <57225309+Anoirwork@users.noreply.github.com>

--- src/firebase/firestore/addData.ts
+++ src/firebase/firestore/addData.ts
@@ -1,5 +1,5 @@
 import firebase_app from "../config";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, setDoc } from "firebase/firestore/lite";
 
 // Get the Firestore instance
 const db = getFirestore(firebase_app);
