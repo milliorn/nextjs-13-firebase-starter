b43deeab96cd32762bbba2485727e536e5a9638b
gonza <gonzalo.muscolo@gmail.com>
2024-06-18 17:16:37 -0300

    more code

--- src/app/sections/index.tsx
+++ src/app/sections/index.tsx
@@ -1,22 +1,27 @@
 import { CloseIcon, EditIcon } from '@chakra-ui/icons';
 import { CloseButton, Flex, Spacer, Box, Card, CardBody, Stack, Heading, Text, CardFooter, ButtonGroup, Button, IconButton } from '@chakra-ui/react'
+import SectionModal from './SectionModal'
 
 const Sections = () => {  
   return(
-    <Flex>
+    <>
+      <Button>Agregar Seccion</Button>
+      <Flex>
         <Box w='15' h='10' />
         <Spacer />
-      <Box borderRadius='md' w='50%' px={4} border="1px solid grey" >
-        <Flex>
+        <Box borderRadius='md' w='50%' px={4} border="1px solid grey" >
+          <Flex>
             <Heading margin={1} size={'md'}>seccion 1</Heading>
             <Spacer/>
             <IconButton aria-label="Editar" margin='1' size={'sm'} icon={<EditIcon />} />
             <IconButton aria-label="Close" margin='1' size={'sm'} icon={<CloseIcon />} />
-        </Flex>
-      </Box>
-      <Spacer />
-      <Box w='15' h='10' />
-    </Flex>
+          </Flex>
+        </Box>
+        <Spacer />
+        <Box w='15' h='10' />
+      </Flex>
+      <SectionModal />
+    </>
   )
 }
 
